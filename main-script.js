7cc73da3dcee1c9cbed23d9c200be230882d427c
R <2075924@cognizant.com>
2022-07-25 14:06:24 +0530

    first commit

---
+++ main-script.js
@@ -0,0 +1,10 @@
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
